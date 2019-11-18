4be9500970e9c330889b26facb9da6900f9c1752
yingying <rhying8@163.com>
2019-11-18 16:48:50 +0800

    添加了666

--- src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -8,6 +8,6 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 public class WebSocketConfig {
     @Bean
     public ServerEndpointExporter serverEndpointExporter(){
-        return new ServerEndpointExporter();
+        return new ServerEndpointExporter();//66666
     }
 }
